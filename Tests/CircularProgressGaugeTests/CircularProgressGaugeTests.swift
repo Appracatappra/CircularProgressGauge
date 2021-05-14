232473c3b8d08d498ad04ba6f97de0a613226583
Kevin Mullins <kevin_mullins@appracatappra.com>
2021-05-14 11:38:59 -0500

    Update utilities & Unit Tests
    Updated to the latest `SwiftletUtilities` and added Unit Tests.

---
+++ Tests/CircularProgressGaugeTests/CircularProgressGaugeTests.swift
@@ -0,0 +1,19 @@
+//
+//  CircularProgressGaugeTests.swift
+//  
+//
+//  Created by Kevin Mullins on 5/14/21.
+//
+
+import XCTest
+import SwiftUI
+@testable import CircularProgressGauge
+
+final class SwiftUtilitiesTests: XCTestCase {
+    
+    func testCircularProgressGauge() {
+        @State var n:Float = 0.5
+        let gauge = CircularProgressGauge(progress: $n)
+        XCTAssert(gauge != nil)
+    }
+}
